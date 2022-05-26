d5764ffff7ba1b19810bd796c0aad4869aecf256
Evka K <kuklaewa@gmail.com>
2022-05-26 11:32:52 +0000

    add active class link to navbar buttons

--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,23 @@
-const sections = document.querySelectorAll('.section');
+const currentLocation = location.href;
+const menuItem = document.querySelectorAll('a');
+const menuLenght = menuItem.length
+for (let i = 0; i<menuLenght; i++){
+    if(menuItem[i].href === currentLocation){
+        menuItem[i].className = "active"
+    }
+}
+
+
+
+/*const activePage = window.location.pathname;
+const navLinks = document.querySelectorAll('nav a').
+forEach(link => {
+    if(link.href.includes(`${activePage}`)){
+        link.classList.add('active');
+    }
+})*/
+
+/*const sections = document.querySelectorAll('.section');
 const sectBtns = document.querySelectorAll('.icon-bar');
 const sectBtn = document.querySelectorAll('.nav-icon');
 const allSections = document.querySelector('.main-content');
@@ -8,9 +27,9 @@ function PageTransitions() {
     //Button click active class
     for (let i = 0; i < sectBtn.length; i++) {
         sectBtn[i].addEventListener('click', function() {
-            let currentBtn = document.querySelectorAll('.active-btn');
-            currentBtn[0].className = currentBtn[0].className.replace('active-btn', '');
-            this.className += ' active-btn';
+            let currentBtn = document.querySelectorAll('.active');
+            currentBtn[0].className = currentBtn[0].className.replace('active', '');
+            this.className += ' active';
         })
     }
 
@@ -35,4 +54,25 @@ function PageTransitions() {
     })
 }
 
-PageTransitions();
+PageTransitions();*/
+
+/*
+const links = document.querySelectorAll('.link');
+const sections = document.querySelectorAll('.section');
+
+let activeLink = 0;
+
+links.forEach((link, i) => {
+    link.addEventListener('click', () => {
+        if (activeLink != i) {
+            links[activeLink].classList.remove('active');
+            link.classList.add('active');
+            sections[activeLink].classList.remove('active');
+
+            setTimeout(() => {
+                activeLink = i;
+                sections[i].classList.add('active');
+            }, 1000);
+        }
+    })
+})*/
